efeb8197c571d6cdd892a210d027095103d79575
Ghanem <ahmed_ghanem2564@yahoo.com~>
2024-10-09 21:18:54 +0300

    Docker-groovy files

--- script.groovy
+++ script.groovy
@@ -33,7 +33,7 @@ def buildNodeApp() {
 
 def buildImage() {
     echo "Building the Docker image..."
-   // withCredentials([usernamePassword(credentialsId: 'a2563d42-a9fe-45f7-974d-e9311e56fec2', passwordVariable: 'PASS', usernameVariable: 'USER')])
+    withCredentials([usernamePassword(credentialsId: 'Dockeraut', passwordVariable: 'PASS', usernameVariable: 'USER')])
         sh "docker build -t ghanemovic/depi-final-project:latest ."
         sh "echo $PASS| docker login -u $USER --password-stdin"
         sh "docker push ghanemovic/depi-final-project:latest"
