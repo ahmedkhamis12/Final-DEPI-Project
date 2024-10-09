a5195066213a043aeba5af4167f8902cbd9785fc
Nourhan Ashour <165235244+Nourhan1227@users.noreply.github.com>
2024-10-10 02:35:55 +0300

    test script.groovy

--- script.groovy
+++ script.groovy
@@ -1,49 +1,30 @@
-// def buildJar() {
-//     echo "building Jar file..."
-//     sh 'mvn package'
-// } 
-
-// def buildImage() {
-//     echo "building the docker image..."
-//     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//     sh 'docker build -t ghanemovic/jenkins-task:jm-.0 .'
-//     sh "echo $PASS | docker login -u $USER --password-stdin"
-//     sh 'docker push ghanemovic/jenkins-task:jm-3.0'
-//     }
-// } 
-
-// def deployApp() {
-//     steps {
-//         echo 'deploying the application...'
-//     }
-// }
-
-return this
-
+// script.groovy
 
 def buildNodeApp() {
     echo "Building Node.js application..."
-    // Navigate to the subdirectory if necessary
-    dir('app') {  // Replace with your actual directory
+    dir('app') {
         sh 'npm install'
-        
     }
 }
 
+def runTests() {
+    echo "Running tests..."
+    dir('app') {
+        sh 'npm test'  // This will generate the JUnit-compatible XML report
+    }
+}
 
 def buildImage() {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'Dockeraut', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t ghanemovic/depi-final-project:latest ."
-        sh "echo $PASS| docker login -u $USER --password-stdin"
+        sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push ghanemovic/depi-final-project:latest"
     }
-    }
-
+}
 
 def deployApp() {
     echo "Deploying the application..."
-    
 }
 
 return this
