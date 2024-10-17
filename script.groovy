c7e140d15523aad3afeb37ca44e3c8d48262bc77
Ghanem <ahmed_ghanem2564@yahoo.com~>
2024-10-17 20:33:11 +0300

    update Jenkins and groovy

--- script.groovy
+++ script.groovy
@@ -1,21 +1,47 @@
-def buildJar() {
-    echo "building Jar file..."
-    sh 'mvn package'
-} 
+// def buildJar() {
+//     echo "building Jar file..."
+//     sh 'mvn package'
+// } 
+
+// def buildImage() {
+//     echo "building the docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//     sh 'docker build -t ghanemovic/jenkins-task:jm-.0 .'
+//     sh "echo $PASS | docker login -u $USER --password-stdin"
+//     sh 'docker push ghanemovic/jenkins-task:jm-3.0'
+//     }
+// } 
+
+// def deployApp() {
+//     steps {
+//         echo 'deploying the application...'
+//     }
+// }
+
+// return this
+
+
+def buildNodeApp() {
+    echo "Building Node.js application..."
+    // Navigate to the subdirectory if necessary
+    dir('app') {  // Replace with your actual directory
+        sh 'npm install'
+    }
+}
+
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t ghanemovic/jenkins-task:jm-.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push ghanemovic/jenkins-task:jm-3.0'
+    echo "Building the Docker image..."
+    withCredentials([usernamePassword(credentialsId: 'Dockeraut', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t ghanemovic/depi-final-project:latest ."
+        sh "echo $PASS| docker login -u $USER --password-stdin"
+        sh "docker push ghanemovic/depi-final-project:latest"
     }
-} 
+}
 
 def deployApp() {
-    steps {
-        echo 'deploying the application...'
-    }
+    echo "Deploying the application..."
+    
 }
 
 return this
